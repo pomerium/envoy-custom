1ee4bf362e98393c0ff48e4ec59203812fc0f22f
Joe Kralicky <joekralicky@gmail.com>
2025-05-19 21:50:22 +0000

    ssh: add packet cipher abstractions and common logic

---
+++ source/extensions/filters/network/ssh/packet_cipher.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <cstdint>
+
+#pragma clang unsafe_buffer_usage begin
+#include "envoy/buffer/buffer.h"
+#pragma clang unsafe_buffer_usage end
+#include "source/common/factory.h"
+#include "source/extensions/filters/network/ssh/openssh.h"
+
+namespace Envoy::Extensions::NetworkFilters::GenericProxy::Codec {
+
+class DirectionalPacketCipher {
+public:
+  virtual ~DirectionalPacketCipher() = default;
+  // Decrypts one packet from the input buffer and writes the plaintext to the output buffer.
+  // Drains bytes from the input buffer.
+  // Returns the number of bytes *decrypted*, which will usually be less than the number of
+  // bytes read.
+  virtual absl::StatusOr<size_t> decryptPacket(uint32_t seqnum, Envoy::Buffer::Instance& out,
+                                               Envoy::Buffer::Instance& in) PURE;
+  virtual absl::Status encryptPacket(uint32_t seqnum, Envoy::Buffer::Instance& out,
+                                     Envoy::Buffer::Instance& in) PURE;
+  virtual size_t blockSize() const PURE;
+  virtual size_t aadLen() const PURE;
+};
+
+using iv_type = bytes;
+using key_type = bytes;
+using mac_type = bytes;
+
+struct DerivedKeys {
+  bytes iv;
+  bytes key;
+  bytes mac;
+};
+
+class DirectionalPacketCipherFactory {
+public:
+  virtual ~DirectionalPacketCipherFactory() = default;
+  virtual std::vector<std::pair<std::string, priority_t>> names() const PURE;
+  virtual std::unique_ptr<DirectionalPacketCipher> create(const DerivedKeys& keys,
+                                                          const DirectionAlgorithms& algs,
+                                                          openssh::CipherMode mode) const PURE;
+  virtual size_t ivSize() const PURE;
+  virtual size_t keySize() const PURE;
+};
+
+class DirectionalPacketCipherFactoryRegistry : public PriorityAwareFactoryRegistry<DirectionalPacketCipherFactory,
+                                                                                   DirectionalPacketCipher,
+                                                                                   const DerivedKeys&,
+                                                                                   const DirectionAlgorithms&,
+                                                                                   openssh::CipherMode> {};
+using DirectionalPacketCipherFactoryPtr = std::unique_ptr<DirectionalPacketCipherFactory>;
+
+class PacketCipher {
+public:
+  PacketCipher(std::unique_ptr<DirectionalPacketCipher> read,
+               std::unique_ptr<DirectionalPacketCipher> write);
+
+  absl::Status encryptPacket(uint32_t seqnum, Envoy::Buffer::Instance& out,
+                             Envoy::Buffer::Instance& in);
+  absl::StatusOr<size_t> decryptPacket(uint32_t seqnum, Envoy::Buffer::Instance& out,
+                                       Envoy::Buffer::Instance& in);
+  size_t blockSize(openssh::CipherMode mode);
+  size_t aadSize(openssh::CipherMode mode);
+  size_t rekeyAfterBytes(openssh::CipherMode mode);
+
+private:
+  std::unique_ptr<DirectionalPacketCipher> read_;
+  std::unique_ptr<DirectionalPacketCipher> write_;
+};
+
+class NoCipher final : public DirectionalPacketCipher {
+public:
+  NoCipher() = default;
+  absl::StatusOr<size_t> decryptPacket(uint32_t /*seqnum*/, Envoy::Buffer::Instance& out,
+                                       Envoy::Buffer::Instance& in) override;
+  absl::Status encryptPacket(uint32_t /*seqnum*/, Envoy::Buffer::Instance& out,
+                             Envoy::Buffer::Instance& in) override;
+  size_t blockSize() const override {
+    // Minimum block size is 8 according to RFC4253 § 6
+    return 8;
+  }
+  size_t aadLen() const override { return 0; }
+};
+
+} // namespace Envoy::Extensions::NetworkFilters::GenericProxy::Codec
---
+++ test/extensions/filters/network/ssh/test_mocks.cc
@@ -0,0 +1,10 @@
+#include "test/extensions/filters/network/ssh/test_mocks.h"
+
+namespace Envoy::Extensions::NetworkFilters::GenericProxy::Codec {
+namespace test {
+
+MockDirectionalPacketCipher::MockDirectionalPacketCipher() {}
+MockDirectionalPacketCipher::~MockDirectionalPacketCipher() {}
+
+} // namespace test
+} // namespace Envoy::Extensions::NetworkFilters::GenericProxy::Codec
